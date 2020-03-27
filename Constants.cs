392ef4d72faf671883b406c1697b0ffd53e3cb7c
grandmastar27 <grandmastar27@gmail.com>
2020-03-27 17:55:11 +0100

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace PasswordGenerator
+{
+    internal static class Constants
+    {
+        public const string _QuestionPasswordLength = "Password length: ";
+        public const string _ErrorMsgMinimumPasswordLength = "The minimum length of password is 8!";
+        public const string _ErrorMsgMaximumPasswordLength = "The maximum length of password is 32!";
+        public const string _ErrorMsgOnlyNumber = "Please input only number(s)!";
+        public const string _QuestionIncludeSymbols = "Include symbols? ";
+        public const string _Yes = "yes";
+        public const string _No = "no";
+        public const string _QuestionYesOrNo = "Yes or no?";
+        public const string _CorrectMistakes = "Please correct your mistake!";
+        public const string _QuestionIncludeNumbers = "Include numbers? ";
+        public const string _QuestionIncludeLowercaseCharacters = "Include lowercase characters? ";
+        public const string _QuestionIncludeUppercaseCharacters = "Include uppercase characters? ";
+        public const string _QuestionSavePassword = "Do you want to save this password into file? ";
+        public const string _MsgSavedPassword = "Saved your password.";
+        public const string _SavingFileName = "saved-password.txt";
+    }
+}
