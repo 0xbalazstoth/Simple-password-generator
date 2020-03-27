4c7eb11f41b690874e5b58bf24327f7a9cf53a8a
grandmastar27 <grandmastar27@gmail.com>
2020-03-27 17:55:08 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            User usr = new User();
+            usr.InputPasswordLength();
+            usr.IncludeSymbols();
+            usr.IncludeNumbers();
+            usr.IncludeLowercaseCharacters();
+            usr.IncludeUppercaseCharacters();
+            usr.SavePassword();
+            usr.Generating();
+
+            Console.ReadKey();
+        }
+    }
+}
